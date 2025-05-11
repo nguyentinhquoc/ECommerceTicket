813360991954b49fcd262b3fa9cea0078599e450
thaiph <thaiphph52804@gmail.com>
2025-05-11 20:03:38 +0700

    update

--- ECommerceTicket2/Controllers/HomeController.cs
+++ ECommerceTicket2/Controllers/HomeController.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ECommerceTicket2.Models;
 namespace ECommerceTicket2.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            
             return View();
         }
 
         public ActionResult Flight()
         {
+            return View();
+        }
+        public ActionResult FlightList()
+        {
+
+            return View();
+        }
+        public ActionResult FlightPayment()
+        {
+
+            return View();
+        }
+        public ActionResult FlightPayment1()
+        {
+
             return View();
         }
     }
